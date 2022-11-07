39fb8472ea65eb6cf2f9f3b34b52cd91d1f13efa
Fernando Marques <frndoliveira@gmail.com>
2022-11-07 11:36:43 -0300

    adjust home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos' },
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular' },
+    { icon: 'wallet-outline', text: 'Depositar' },
+    { icon: 'options-outline', text: 'Ajustar limite' },
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'barcode-outline', text: 'Pagar' },
+    { icon: 'lock-open-outline', text: 'Bloquear cartão' },
+    { icon: 'card-outline', text: 'Cartão virtual' },
+  ];
+
+  public slidesOptions: any = {
+    slidesPerView: 3,
+    freeMode: true,
+  };
 
   constructor() {}
-
 }
